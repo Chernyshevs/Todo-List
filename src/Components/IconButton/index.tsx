0522ce41bd2d860f9f764a78a09a3c50500c62ab
purplecodev <cstepan345@gmail.com>
2025-03-16 23:13:08 +0300

    delete extra files

--- src/Components/IconButton/index.tsx
+++ src/Components/IconButton/index.tsx
@@ -1,5 +1,19 @@
-export default function IconButton({ color, children, ...props }) {
-  return (
-    <button className={`btn-card ${color}`} {...props}><img src={children} alt="button icon" /></button>
-  )
+import React from "react";
+
+interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color: string;
+  children: React.ReactNode;
 }
+const IconButton: React.FC<IconButtonProps> = ({ color, children, ...props }) => {
+  return (
+    <button className={`btn-card ${color}`} {...props}>
+      {typeof children === "string" ? (
+        <img src={children} alt="button icon" />
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
+
+export default IconButton;
