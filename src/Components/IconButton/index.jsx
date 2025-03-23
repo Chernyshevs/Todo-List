73bbf89ef2983cbb50b866d0ba887dc688cddd81
purplecodev <cstepan345@gmail.com>
2025-03-23 15:19:38 +0300

    delete jsx files

--- src/Components/IconButton/index.jsx
+++
@@ -1,5 +0,0 @@
-export default function IconButton({ color, children, ...props }) {
-  return (
-    <button className={`btn-card ${color}`} {...props}><img src={children} alt="button icon" /></button>
-  )
-}
