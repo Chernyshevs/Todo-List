043002dcd7a5a38d066f1b13ec55d6abed26ac57
purplecodev <cstepan345@gmail.com>
2025-03-27 00:20:38 +0300

    memo in addTask

--- src/Components/AddTask/index.tsx
+++ src/Components/AddTask/index.tsx
@@ -3,64 +3,65 @@ import { Form, Input, Button } from "antd";
 import type { FormProps } from "antd";
 import type { FieldNameTask } from "../../types/todoTypes";
 import { MIN_TAKS_LENGTH, MAX_TAKS_LENGTH } from "../../constants/todos";
-const AddTask: React.FC<{ fetchTasks: () => Promise<void> }> = ({
-  fetchTasks,
-}) => {
-  const [form] = Form.useForm();
-  const handleAddTask: FormProps<FieldNameTask>["onFinish"] = async (
-    values
-  ) => {
-    try {
-      await addTask({ title: values.taskname?.trim(), isDone: false });
-      await fetchTasks();
-    } catch (error: any) {
-      alert(`Ошибка: ${error.message}`);
-    } finally {
-      form.resetFields();
-    }
-  };
-  const onFinishFailed: FormProps<FieldNameTask>["onFinishFailed"] = (
-    errorInfo
-  ) => {
-    console.log("Failed:", errorInfo);
-  };
+import { memo } from "react";
+const AddTask: React.FC<{ fetchTasks: () => Promise<void> }> = memo(
+  ({ fetchTasks }) => {
+    const [form] = Form.useForm();
+    const handleAddTask: FormProps<FieldNameTask>["onFinish"] = async (
+      values
+    ) => {
+      try {
+        await addTask({ title: values.taskname?.trim(), isDone: false });
+        await fetchTasks();
+      } catch (error: any) {
+        alert(`Ошибка: ${error.message}`);
+      } finally {
+        form.resetFields();
+      }
+    };
+    const onFinishFailed: FormProps<FieldNameTask>["onFinishFailed"] = (
+      errorInfo
+    ) => {
+      console.log("Failed:", errorInfo);
+    };
 
-  return (
-    <section>
-      <Form
-        name="basic"
-        layout="inline"
-        style={{ justifyContent: "center" }}
-        onFinish={handleAddTask}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        form={form}
-      >
-        <Form.Item<FieldNameTask>
-          style={{ width: "55%" }}
-          name="taskname"
-          rules={[
-            { required: true, message: "Пожалуйста введите имя задачи!" },
-            {
-              min: MIN_TAKS_LENGTH,
-              message: `Имя задачи должно быть минимум ${MIN_TAKS_LENGTH} символа`,
-            },
-            {
-              max: MAX_TAKS_LENGTH,
-              message: `Имя задачи должно быть максимум ${MAX_TAKS_LENGTH} символов`,
-            },
-          ]}
+    return (
+      <section>
+        <Form
+          name="basic"
+          layout="inline"
+          style={{ justifyContent: "center" }}
+          onFinish={handleAddTask}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          form={form}
         >
-          <Input placeholder="Название задачи" size="large" />
-        </Form.Item>
+          <Form.Item<FieldNameTask>
+            style={{ width: "55%" }}
+            name="taskname"
+            rules={[
+              { required: true, message: "Пожалуйста введите имя задачи!" },
+              {
+                min: MIN_TAKS_LENGTH,
+                message: `Имя задачи должно быть минимум ${MIN_TAKS_LENGTH} символа`,
+              },
+              {
+                max: MAX_TAKS_LENGTH,
+                message: `Имя задачи должно быть максимум ${MAX_TAKS_LENGTH} символов`,
+              },
+            ]}
+          >
+            <Input placeholder="Название задачи" size="large" />
+          </Form.Item>
 
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit" size="large">
-            Добавить
-          </Button>
-        </Form.Item>
-      </Form>
-    </section>
-  );
-};
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit" size="large">
+              Добавить
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
+    );
+  }
+);
 export default AddTask;
