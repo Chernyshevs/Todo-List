d2af38e5fa3b85a910a41bb43bc4bdf5a26d87d7
purplecodev <cstepan345@gmail.com>
2025-04-04 17:05:05 +0300

    delete un components

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,30 +1,125 @@
-import AuthLayout from "../../Components/AuthLayout";
-import AuthForm from "../../Components/AuthForm";
-import { Link } from "react-router-dom";
+import "./LoginPage.css";
+
+import authDesignElement from "../../assets/auth-element.png";
+import { Button, Form, Input } from "antd";
+import { ConfigProvider } from "antd";
+import { authValidation } from "../../constants/auth";
 
 import type { AuthData } from "../../types/authTypes";
+import type { FormProps } from "antd";
+import { Link } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
-  const onSubmitLogin = (values: AuthData) => {
-    console.log("OK!" + values);
+  const onSubmitLogin: FormProps<AuthData>["onFinish"] = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed: FormProps<AuthData>["onFinishFailed"] = (errorInfo) => {
+    console.log("Failed:", errorInfo);
   };
 
-  const titleData = (
-    <>
-      <h1>Войдите в аккаунт</h1>
-      <p>Посмотрите, что происходит с вашим бизнесом</p>
-    </>
-  );
-
-  const footerData = (
-    <p>
-      Не зарегистрированы? <Link to="/auth/registration">Создать аккаунт</Link>
-    </p>
-  );
   return (
-    <AuthLayout titleData={titleData} footerData={footerData}>
-      <AuthForm<AuthData> type="login" onSubmit={onSubmitLogin} />
-    </AuthLayout>
+    <div className="login-all-content">
+      <main className="login-main-content">
+        <div className="login-container">
+          <img
+            src={authDesignElement}
+            alt="Дизайн элемент авторизации"
+            className="login-design-img"
+          />
+          <div className="login-title">
+            <h1>Войдите в аккаунт</h1>
+            <p>Посмотрите, что происходит с вашим бизнесом</p>
+          </div>
+          <div className="login-form">
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: "#7F265B",
+                },
+              }}
+            >
+              <Form
+                name="basic"
+                wrapperCol={{ span: 16 }}
+                initialValues={{ remember: true }}
+                onFinish={onSubmitLogin}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                layout="vertical"
+                className="login-form"
+              >
+                <Form.Item<AuthData>
+                  label="Логин"
+                  name="login"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста введите ваш логин",
+                    },
+                    {
+                      min: authValidation.MIN_LOGIN_LENGHT,
+                      message: `Минимум ${authValidation.MIN_LOGIN_LENGHT} символов`,
+                    },
+                    {
+                      max: authValidation.MAX_LOGIN_LENGHT,
+                      message: `Максимум ${authValidation.MAX_LOGIN_LENGHT} символов`,
+                    },
+                  ]}
+                >
+                  <Input
+                    size="large"
+                    className="login-input"
+                    placeholder="Логин"
+                  />
+                </Form.Item>
+                <Form.Item<AuthData>
+                  label="Пароль"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста введите ваш пароль!",
+                    },
+                    {
+                      min: authValidation.MIN_PASSWORD_LENGHT,
+                      message: `Пароль минимум ${authValidation.MIN_PASSWORD_LENGHT} символов`,
+                    },
+                    {
+                      max: authValidation.MAX_PASSWORD_LENGHT,
+                      message: `Пароль максимум ${authValidation.MAX_PASSWORD_LENGHT} символов`,
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    size="large"
+                    className="login-input"
+                    placeholder="*****************"
+                  />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    size="large"
+                    className="login-form-btn"
+                  >
+                    Войти
+                  </Button>
+                </Form.Item>
+              </Form>
+            </ConfigProvider>
+          </div>
+        </div>
+      </main>
+      <footer className="login-footer">
+        {" "}
+        <p>
+          Не зарегистрированы?{" "}
+          <Link to="/auth/registration">Создать аккаунт</Link>
+        </p>
+      </footer>
+    </div>
   );
 };
 
--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -1,33 +1,225 @@
-import AuthLayout from "../../Components/AuthLayout";
-import AuthForm from "../../Components/AuthForm";
-import { Link } from "react-router-dom";
+import "./RegistrationPage.css";
+import PhoneInput from "antd-phone-input";
+import authDesignElement from "../../assets/auth-element.png";
+import { Button, Form, Input } from "antd";
+import { ConfigProvider } from "antd";
+import { authValidation } from "../../constants/auth";
 
 import type { UserRegistration } from "../../types/authTypes";
+import type { FormProps } from "antd";
+import { Link } from "react-router-dom";
 
 const RegistrationPage: React.FC = () => {
-  const onSubmitRegistraion = (values: UserRegistration) => {
-    console.log("OK!" + values);
+  const onSubmitRegistration: FormProps<UserRegistration>["onFinish"] = (
+    values
+  ) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed: FormProps<UserRegistration>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
   };
 
-  const titleData = (
-    <>
-      <h1>Создайте аккаунт</h1>
-      <p>Посмотрите, что происходит с вашим бизнесом</p>
-    </>
-  );
-
-  const footerData = (
-    <p>
-      Уже есть аккаунт? <Link to="/auth/login">Войти в свой аккаунт</Link>
-    </p>
-  );
   return (
-    <AuthLayout titleData={titleData} footerData={footerData}>
-      <AuthForm<UserRegistration>
-        type="registration"
-        onSubmit={onSubmitRegistraion}
-      />
-    </AuthLayout>
+    <div className="reg-all-content">
+      <main className="reg-main-content">
+        <div className="reg-container">
+          <img
+            src={authDesignElement}
+            alt="Дизайн элемент авторизации"
+            className="reg-design-img"
+          />
+          <div className="reg-title">
+            <h1>Войдите в аккаунт</h1>
+            <p>Посмотрите, что происходит с вашим бизнесом</p>
+          </div>
+          <div className="reg-form">
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: "#7F265B",
+                },
+              }}
+            >
+              <Form
+                name="basic"
+                wrapperCol={{ span: 16 }}
+                initialValues={{ remember: true }}
+                onFinish={onSubmitRegistration}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                layout="vertical"
+                className="reg-form"
+              >
+                <Form.Item<UserRegistration>
+                  label="Логин"
+                  name="login"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста введите ваш логин",
+                    },
+                    {
+                      min: authValidation.MIN_LOGIN_LENGHT,
+                      message: `Минимум ${authValidation.MIN_LOGIN_LENGHT} символов`,
+                    },
+                    {
+                      max: authValidation.MAX_LOGIN_LENGHT,
+                      message: `Максимум ${authValidation.MAX_LOGIN_LENGHT} символов`,
+                    },
+                  ]}
+                >
+                  <Input
+                    size="large"
+                    className="reg-input"
+                    placeholder="Логин"
+                  />
+                </Form.Item>
+                <Form.Item<UserRegistration>
+                  label="Имя пользователя"
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста введите ваше имя пользователя",
+                    },
+                    {
+                      min: authValidation.MIN_USERNAME_LENGTH,
+                      message: `Минимум ${authValidation.MIN_USERNAME_LENGTH} символов`,
+                    },
+                    {
+                      max: authValidation.MAX_USERNAME_LENGTH,
+                      message: `Максимум ${authValidation.MAX_USERNAME_LENGTH} символов`,
+                    },
+                  ]}
+                >
+                  <Input
+                    size="large"
+                    className="reg-input"
+                    placeholder="Имя пользователя"
+                  />
+                </Form.Item>
+                <Form.Item<UserRegistration>
+                  label="Пароль"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста введите ваш пароль!",
+                    },
+                    {
+                      min: authValidation.MIN_PASSWORD_LENGHT,
+                      message: `Пароль минимум ${authValidation.MIN_PASSWORD_LENGHT} символов`,
+                    },
+                    {
+                      max: authValidation.MAX_PASSWORD_LENGHT,
+                      message: `Пароль максимум ${authValidation.MAX_PASSWORD_LENGHT} символов`,
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    size="large"
+                    className="reg-input"
+                    placeholder="*****************"
+                  />
+                </Form.Item>
+                <Form.Item<UserRegistration>
+                  label="Повторите пароль"
+                  name="passwordTwo"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста повторите ваш пароль!",
+                    },
+                    {
+                      min: authValidation.MIN_PASSWORD_LENGHT,
+                      message: `Пароль минимум ${authValidation.MIN_PASSWORD_LENGHT} символов`,
+                    },
+                    {
+                      max: authValidation.MAX_PASSWORD_LENGHT,
+                      message: `Пароль максимум ${authValidation.MAX_PASSWORD_LENGHT} символов`,
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        return value && value === getFieldValue("password")
+                          ? Promise.resolve()
+                          : Promise.reject(new Error("Пароли не совпадают!"));
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password
+                    size="large"
+                    className="reg-input"
+                    placeholder="*****************"
+                  />
+                </Form.Item>
+
+                <Form.Item<UserRegistration>
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Пожалуйста введите ваш Email!",
+                    },
+                    {
+                      type: "email",
+                      message: "Email введён неверно!",
+                    },
+                  ]}
+                >
+                  <Input
+                    size="large"
+                    className="reg-input"
+                    placeholder="mail@abc.com"
+                  />
+                </Form.Item>
+
+                <Form.Item<UserRegistration>
+                  label="Мобильный телефон"
+                  name="phoneNumber"
+                  rules={[
+                    {
+                      validator(_, { valid }) {
+                        if (valid()) return Promise.resolve();
+                        return Promise.reject("Неверный формат телефона!");
+                      },
+                    },
+                  ]}
+                >
+                  <PhoneInput
+                    size="large"
+                    className="reg-input"
+                    placeholder="+7 (800) 555 3535"
+                    enableSearch
+                  />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    size="large"
+                    className="reg-form-btn"
+                  >
+                    Зарегистрироваться
+                  </Button>
+                </Form.Item>
+              </Form>
+            </ConfigProvider>
+          </div>
+        </div>
+      </main>
+      <footer className="reg-footer">
+        {" "}
+        <p>
+          Уже есть аккаунт? <Link to="/auth/login">Войти</Link>
+        </p>
+      </footer>
+    </div>
   );
 };
+
 export default RegistrationPage;
